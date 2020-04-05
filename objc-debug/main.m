9aac6a4dc6382c81825fd2a53089176acf148f02
FyhSky <fengyinghaotjut@126.com>
2020-04-05 14:52:22 +0800

    compile success.

---
+++ objc-debug/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  objc-debug
+//
+//  Created by FengYinghao on 2020/4/5.
+//
+
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        NSObject *objc = [NSObject alloc];
+        
+    }
+    return 0;
+}
